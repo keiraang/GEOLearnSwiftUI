7ac3d745cfe04fe72803773cf3d788530607e68d
kiranerys <keira.nerys.ang@gmail.com>
2023-06-27 14:20:00 -0400

    Initial Commit

---
+++ GEOLearn/GEOLearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  GEOLearnApp.swift
+//  GEOLearn
+//
+//  Created by Keira Ang on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct GEOLearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
