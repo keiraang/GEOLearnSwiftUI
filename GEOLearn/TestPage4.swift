cf31240816f070d03ab2bb62bcf92cc9463cd7f5
Kam <kamaralchima@gmail.com>
2023-06-30 09:27:50 -0400

    added TestPage4 file for app to run, used stimulator for script purposes and demo recording

---
+++ GEOLearn/TestPage4.swift
@@ -0,0 +1,20 @@
+//
+//  TestPage4.swift
+//  GEOLearn
+//
+//  Created by Kamara Chima on 6/30/23.
+//
+
+import SwiftUI
+
+struct TestPage4: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TestPage4_Previews: PreviewProvider {
+    static var previews: some View {
+        TestPage4()
+    }
+}
