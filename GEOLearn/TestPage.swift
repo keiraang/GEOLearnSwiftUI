12d5d6a9899d23e300ee0b0f13c4516a1b1a9387
kiranerys <keira.nerys.ang@gmail.com>
2023-06-30 11:14:04 -0400

    construction page

--- GEOLearn/TestPage.swift
+++ GEOLearn/TestPage.swift
@@ -11,34 +11,18 @@ struct TestPage: View {
     var body: some View {
         NavigationStack {
             ZStack {
+                Color(hue: 0.0, saturation: 0.0, brightness: 0.727)
+                    .ignoresSafeArea()
                 VStack {
-                    
-                    Text("Image placeholder 1")
-                        .font(.title2)
-                    //this is where the image will go
-                }.toolbar {
-                    //this is the Navbar setup of the app
-                    NavigationLink(destination:TestPage()) {
-                        Text("1")
-                            .fontWeight(.bold)
-                    }
-                    NavigationLink(destination:TestPage2()) {
-                        Text("2")
-                            .fontWeight(.bold)
-                    }
-                    NavigationLink(destination:TestPage3()) {
-                        Text("3")
-                            .fontWeight(.bold)
-                        
-                    }
-                    NavigationLink(destination:TestPage4()) {
-                        Text("4")
-                            .fontWeight(.bold)
-                        
-                    }
+                    Image("construction")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width:350)
+                    Text("Sorry! This page is still being built. Thank you for your patience.")
+                        .font(.title)
+                        .fontWeight(.semibold)
+                        .padding(/*@START_MENU_TOKEN@*/.all, 40.0/*@END_MENU_TOKEN@*/)
                 }
-                .navigationBarBackButtonHidden(true)
-                
             }
         }
     }
